808b37c0f34332338c1378f1779435cecd664e9c
Nihal Dayal <nihaldayal@gmail.com>
2021-07-09 20:20:21 -0700

    Finished working version

--- bot.py
+++ bot.py
@@ -1,20 +1,24 @@
 import collections
 import json
+import logging
 import random
+import os
 import requests
-import toml
-from os import path
+import boto3
+from botocore.exceptions import ClientError
 
 import praw
 
-CACHE_FILE = "cache.json"
-CACHE_TIMEOUT = 7 * 24 * 60 * 60  # one week
-CREDS_FILE_PATH = "credentials.toml"
 FAB_API_URL = "https://api.fabdb.net"
-FAB_SUBREDDIT = "FleshAndBloodTCG"
+FAB_SUBREDDIT = os.environ.get("SUBREDDIT") or "test"
+SECRET_NAME = "prod/COTDBot/credentials"
+AWS_REGION = "us-east-2"
+
+logger = logging.getLogger("cotd-bot")
+logger.setLevel(logging.INFO)
 
 
-def generate_post_and_title(card) -> (str, str):
+def generate_post(card) -> (str, str):
     post_title = f"Card of The Day - {card['name']}"
     card_link = "https://fabdb.net/cards/" + card['identifier']
     keywords = ", ".join(card['keywords'])
@@ -38,11 +42,37 @@ def generate_post_and_title(card) -> (str, str):
     return (post_title, ''.join(post_lines))
 
 
+def get_secret(secret_name, region_name) -> str:
 
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+                SecretId=secret_name)
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            raise e
+    else:
+        secret = get_secret_value_response['SecretString']
+        return secret
 
 
 def get_handle() -> praw.Reddit:
-    creds = toml.load(CREDS_FILE_PATH)
+    secret = get_secret(SECRET_NAME, AWS_REGION)
+    creds = json.loads(secret)
 
     r = praw.Reddit(user_agent="COTDBOT",
                     username=creds["username"],
@@ -52,53 +82,52 @@ def get_handle() -> praw.Reddit:
     return r
 
 
-def update_cache():
-    payload = {"page": 1, "per_page": 100}
+def fetch_cards():
+    payload = {"page": 1}
     first_resp = requests.get(FAB_API_URL + "/cards",
-                              params=payload, timeout=1).json()
+                              params=payload, timeout=30).json()
 
     num_pages = first_resp["meta"]["last_page"]
-    card_cache = collections.defaultdict()
+    cards = collections.defaultdict()
+    if not num_pages:
+        logger.error("Error getting number of cards from first API request")
+        logger.error(f"Response: {first_resp}")
+        return
+
+    # Iterate through the paginated list of cards
     for i in range(1, num_pages+1):
         url = f"{FAB_API_URL}/cards?page={i}&per_page=100"
-        resp = requests.get(url, timeout=1).json()
+        resp = requests.get(url, timeout=1)
+        if not resp.ok:
+            logger.error("Error getting page {i} of {num_pages}")
+            logger.error("Response Code: {resp.status_code}")
+            continue
+        resp = resp.json()
         data = resp["data"]
-        cards = {card["identifier"]: card for card in data}
-        card_cache.update(cards)
-
-    with open(CACHE_FILE, 'w') as f:
-        json.dump(card_cache, f)
-
-    return card_cache
+        cards_page = {card["identifier"]: card for card in data}
+        cards.update(cards_page)
+    return cards
 
 
-def get_cards():
-    payload = {"page": 1, "per_page": 100}
-    resp = requests.get(FAB_API_URL + "/cards",
-                        params=payload).json()
-    current_num_cards = resp["meta"]["total"]
+def lambda_handler(self, event):
+    all_cards = fetch_cards()
+    if not all_cards:
+        logger.error("No cards returned from FaBDB.net")
+        return
+    logger.info("Picking a random card out of %i", len(all_cards))
+    cotd = random.choice(list(all_cards.values()))
+    title, post = generate_post(cotd)
 
-    # If the cache file doesnt exist, update the cache
-    if not path.exists(CACHE_FILE):
-        return update_cache()
-
-    with open(CACHE_FILE, 'r') as file:
-        card_cache = json.load(file)
-
-    # Refresh cache if the total number of cards have changed.
-    cards_changed = current_num_cards != len(card_cache)
-    if cards_changed:
-        print("Total number of cards has changed. Refreshing cache.")
-        return update_cache()
-    else:
-        print("Total number of cards has remained unchanged. Using old cache.")
-        return card_cache
-
-
-all_cards = get_cards()
-cotd = random.choice(list(all_cards.values()))
-title, post = generate_post_and_title(cotd)
-
-reddit = get_handle()
-sr = reddit.subreddit('test')
-sr.submit(title, selftext=post)
+    try:
+        reddit = get_handle()
+        subreddit = reddit.subreddit(FAB_SUBREDDIT)
+        submission = subreddit.submit(title, selftext=post)
+        return {
+                "saved": submission.saved,
+                "title": submission.title,
+                "content": submission.selftext,
+                "url": submission.permalink,
+                "subreddit": FAB_SUBREDDIT
+               }
+    except Exception as e:
+        logger.error("Exception thrown while getting reddit handle: %s", e)
